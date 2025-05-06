7f426e40e695445bde0e2647ff5c8d9efdf54c2c
Jhean Bryan <93744970+jheanbryan@users.noreply.github.com>
2025-05-06 15:31:29 -0400

    updates

--- src/components/Cadastro/Cadastro.tsx
+++ src/components/Cadastro/Cadastro.tsx
@@ -1,60 +1,61 @@
+import { Link } from 'react-router';
 import './Cadastro.css'
 
 function ContainerIndex() {
   return (
     <>
-
-      <div className="content">
-        <nav>
+      <div className="cadastro-container">
+        <div className="content">
+          <nav>
             <ul>
-                <li><a href="http://www.ifms.edu.br/">Site do IFMS</a></li>
+              <li><a href="http://www.ifms.edu.br/">Site do IFMS</a></li>
             </ul>
-        </nav>
+          </nav>
 
-        <main>
+          <main>
             <h2>Cadastre-se</h2>
 
             <form method="post" action="/home" encType="multipart/form-data">
-                <p>Cadastre-se como
-                    <select name="perfil">
-                        <option value="estudante">Estudante</option>
-                        <option value="professor">Professor</option>
-                    </select>
-                </p>
-                <p>Nome:
-                    <input type="text" name="nome" placeholder="nome" required/>
-                </p>
-                <p>Email:
-                    <input type="email" name="email" placeholder="email" required/>
-                </p>
-                <p id="paiSenha1">Senha:
-                    <input type="password" id="senha1" placeholder="senha" name="senha" required/>
-                </p>
-                <p id="paiSenha2">Repita a senha:
-                    <input type="password" id="senha2" required placeholder="repita a senha" />
-                    
-                </p>
-                <p>Foto:
-                    <input type="file" name="foto" />
-                </p>
-                <p>CPF (somente números):
-                    <input type="tel" placeholder="CPF" name="cpf" required />
-                </p>
-                <p>Data de nascimento (dd/mm/aaaa)
-                    <input type="date" required name="dataNasc" />
-                </p>
-                
-                <button type="submit">Cadastrar</button>
+              <p>Cadastre-se como
+                <select name="perfil">
+                  <option value="estudante">Estudante</option>
+                  <option value="professor">Professor</option>
+                </select>
+              </p>
+              <p>Nome:
+                <input type="text" name="nome" placeholder="nome" required />
+              </p>
+              <p>Email:
+                <input type="email" name="email" placeholder="email" required />
+              </p>
+              <p id="paiSenha1">Senha:
+                <input type="password" id="senha1" placeholder="senha" name="senha" required />
+              </p>
+              <p id="paiSenha2">Repita a senha:
+                <input type="password" id="senha2" required placeholder="repita a senha" />
+              </p>
+              <p>Foto:
+                <input type="file" name="foto" />
+              </p>
+              <p>CPF (somente números):
+                <input type="tel" placeholder="CPF" name="cpf" required />
+              </p>
+              <p>Data de nascimento (dd/mm/aaaa)
+                <input type="date" required name="dataNasc" />
+              </p>
 
+              <button><Link to="/home">Cadastrar</Link></button>
             </form>
-        </main>
-        <section>
+          </main>
+
+          <section>
             <h2>Últimas notícias</h2>
             <p>Aberto processo seletivo</p>
-        </section>
-    </div>
+          </section>
+        </div>
+      </div>
     </>
-  );  
-};
+  );
+}
 
 export default ContainerIndex;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,18 @@
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header(props: any) {
 
   return (
     <div className='container-header'>
       <header  className='header'>
-        <h1>Sistema Acadêmico {props.subtitle}</    h1>
+        <h1>Sistema Acadêmico {props.subtitle}</h1>
 
         {props.mostrarFormulario && (
           <form id="login" action="/home" method="post">
             <input type="email" placeholder="e-mail" name="email" />
             <input type="password" placeholder="senha" id="senha3" name="senha" />
-            <button type="submit">Logar</button>
+            <Link to="/home"><button>Logar</button></Link>
           </form>
         )}
 
